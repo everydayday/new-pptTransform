6a1a13d78a8b6764c3b4675f72448c0034cf19fa
dahee Kim <96685431+everydayday@users.noreply.github.com>
2023-01-16 15:19:27 +0900

    add 'quit' btn

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ class MyApp(QWidget):
         self.qbtn1.setGeometry(650,50,300,100)
         self.qbtn1.clicked.connect(self.button_clicked)
 
+        self.qbtn_1 = QPushButton('종료하기',self)
+        self.qbtn_1.setGeometry(650,450,300,100)
+        self.qbtn_1.clicked.connect(QApplication.instance().quit)
         
         
         self.show()
